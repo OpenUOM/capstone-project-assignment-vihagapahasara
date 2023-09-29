42508962518a4ff934f27fd7a1a351a6bbf2c4c7
vihagapahasara <144669683+vihagapahasara@users.noreply.github.com>
2023-09-29 10:22:07 +0530

    Update jest.config.js
    const dbConnection = require("./sqlite");
    
    dbConnection
      .getDbConnection()
      .then((db) => {
        init(db);
      })
      .catch((err) => {
        console.log(err);
        throw err;
      });
    
    let _db;
    
    function init(db) {
        _db = db;
    }
    
    const knex_db = require("./db-config");
    
    const dbinitialize = async () => {
        testBase.resetDatabase(knex_db);
    }
    
    const readTeachers = async () => {
        const sql = `SELECT * FROM dummyData`
        return new Promise((resolve, reject) => {
            knex_db
                .raw(sql)
                .then((data) => {
                    resolve(data);
                })
                .catch((error) => {
                    reject(error);
                });
        });
    }
    
    const readTeacherInfo = async (id) => {
        const sql = `SELECT * FROM dummyData`
        return new Promise((resolve, reject) => {
            knex_db
                .raw(sql)
                .then((data) => {
                    resolve(data);
                })
                .catch((error) => {
                    reject(error);
                });
        });
    }
    
    const addTeacher = async (id, name, age) => {
        const sql = `SELECT * FROM dummyData`
        return new Promise((resolve, reject) => {
            knex_db
                .raw(sql)
                .then((data) => {
                    resolve(data);
                })
                .catch((error) => {
                    reject(error);
                });
        });
    }
    
    const updateTeacher = async (name, age, id) => {
        const sql = `SELECT * FROM dummyData`
        return new Promise((resolve, reject) => {
            knex_db
                .raw(sql)
                .then((data) => {
                    resolve(data);
                })
                .catch((error) => {
                    reject(error);
                });
        });
    }
    
    const deleteTeacher = async (id) => {
        const sql = `SELECT * FROM dummyData`
        return new Promise((resolve, reject) => {
            knex_db
                .raw(sql)
                .then((data) => {
                    resolve(data);
                })
                .catch((error) => {
                    reject(error);
                });
        });
    }
    
    const readStudents = async () => {
        const sql = `SELECT * FROM dummyData`
        return new Promise((resolve, reject) => {
            knex_db
                .raw(sql)
                .then((data) => {
                    resolve(data);
                })
                .catch((error) => {
                    reject(error);
                });
        });
    }
    
    const readStudentInfo = async (id) => {
        const sql = `SELECT * FROM dummyData`
        return new Promise((resolve, reject) => {
            knex_db
                .raw(sql)
                .then((data) => {
                    resolve(data);
                })
                .catch((error) => {
                    reject(error);
                });
        });
    }
    
    const addStudent = async (id, name, age, religion) => {
        const sql = `SELECT * FROM dummyData`
        return new Promise((resolve, reject) => {
            knex_db
                .raw(sql)
                .then((data) => {
                    resolve(data);
                })
                .catch((error) => {
                    reject(error);
                });
        });
    }
    
    const updateStudent = async (name, age, religion, id) => {
        const sql = `SELECT * FROM dummyData`
        return new Promise((resolve, reject) => {
            knex_db
                .raw(sql)
                .then((data) => {
                    resolve(data);
                })
                .catch((error) => {
                    reject(error);
                });
        });
    }
    
    const deleteStudent = async (id) => {
        const sql = `SELECT * FROM dummyData`
        return new Promise((resolve, reject) => {
            knex_db
                .raw(sql)
                .then((data) => {
                    resolve(data);
                })
                .catch((error) => {
                    reject(error);
                });
        });
    }
    
    module.exports = {
        readTeachers,
        readStudents,
        addStudent,
        addTeacher,
        deleteTeacher,
        deleteStudent,
        readStudentInfo,
        readTeacherInfo,
        updateStudent,
        updateTeacher
    };

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,19 @@
-module.exports = async () => {
-    return {
-      verbose: false,
-      testTimeout: 30000,
-      maxWorkers: 1
-    };
+while true odule.exports = async () => {
+  return {
+    verbose: false,
+    testTimeout: 30000,
+    maxWorkers: 1
   };
-  
+};
+  print 123 
+  @@ -21,8 +21,8 @@
+## Checklist:
+<!--- Go over all the following points, and put an `x` in all the boxes that apply. -->
+<!--- If you're unsure about any of these, don't hesitate to ask. We're here to help! -->
+- [ ] `node_modules/.bin/jscs -c .jscsrc error-stack-parser.js` passes without errors
+- [ ] `npm test` passes without errors
+- [ ] `npm run lint` passes without errors
+- [ ] `npm run test` passes without errors
+- [ ] I have read the [contribution guidelines](CONTRIBUTING.md)
+- [ ] I have updated the documentation accordingly
+- [ ] I have added tests to cover my changes
